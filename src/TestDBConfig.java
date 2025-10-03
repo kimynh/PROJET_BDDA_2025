db03077abb2de99c1a55d240c1935e65639c8b53
talanasrah03 <talanasrah03@gmail.com>
2025-10-03 14:28:09 +0200

    Add TestDBConfig and config.properties for TP2

--- src/TestDBConfig.java
+++ src/TestDBConfig.java
@@ -1,3 +1,8 @@
-public class TestDBConfig {
-    
+ public class TestDBConfig {
+    public static void main(String[] args) throws Exception {
+        DBConfig cfg = DBConfig.LoadDBConfig("src/fichier_conf.json");
+        System.out.println(cfg);
+    }
 }
+
+
