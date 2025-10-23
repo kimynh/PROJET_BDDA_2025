c394202a58c8b813c607ef2d68c5a7cf5f30493b
Elodie Cao <yennhi@MacBook-Air-de-Yen.local>
2025-10-23 13:51:10 +0200

    Modification de l'objet Relation car son constructeur a changé

--- src/TestRelation.java
+++ src/TestRelation.java
@@ -1,11 +1,19 @@
 public class TestRelation {
-    public static void main(String[] args) {
-        Relation student = new Relation("Student");
+    public static void main(String[] args) throws Exception {
+        DBConfig cfg = DBConfig.LoadDBConfig("src/fichier_conf.json");
+        DiskManager dm = new DiskManager(cfg);
+        BufferManager bm = new BufferManager(cfg, dm);
+        dm.Init();
+
+        Relation student = new Relation("Student", dm, bm, cfg);
         student.addColumn("id", "int");
         student.addColumn("name", "string");
         student.addColumn("grade", "float");
 
         System.out.println(student);
         System.out.println("Record size = " + student.getRecordSize());
+
+        dm.Finish();
+        bm.FlushBuffers();
     }
 }
